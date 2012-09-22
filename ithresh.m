43458002b1cdfd12eb6b2c638f6fe6f0508ecda3
Peter Corke <peter.i.corke@gmail.com>
2012-09-22 23:34:31 +0000

    Comments and doco upgrade If output arg is requested then a quit button is displayed and the function is blocking

--- ithresh.m
+++ ithresh.m
@@ -5,10 +5,17 @@
 %
 % ITHRESH(IM, T) as above but the initial threshold is set to T.
 %
+% IM2 = ITHRESH(IM) as above but returns the thresholded image after the
+% "done" button in the GUI is pressed.
+%
+% [IM2,T] = ITHRESH(IM) as above but also returns the threshold value.
+%
 % Notes::
 % - Greyscale image only.
 % - For a uint8 class image the slider range is 0 to 255.
 % - For a floating point class image the slider range is 0 to 1.0
+% - The GUI only displays the "done" button if output arguments are
+%   requested, otherwise the threshold window operates independently.
 %
 % See also IDISP.
 
@@ -30,7 +37,7 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function ithresh(im, t)
+function [imt, thresh] = ithresh(im, t)
 
     % number of colors
     ncolors = 256;
@@ -54,17 +61,20 @@ function ithresh(im, t)
 
     % set up the color map with defined limits
     set(gca, 'CLimMode', 'Manual');
+
+
+    % display the image in a small set of axes
+    h = image(im);
+    set(h, 'CDataMapping', 'scaled');
+    
+    set(gca, 'Units', 'Normalized', ...
+        'Position', [0.13 0.15 0.775 0.75]);
     if isfloat(im)
         set(gca, 'CLim', [0 1]);
     else
         set(gca, 'CLim', [0 intmax(class(im))]);
     end
-
-    % display the image in a small set of axes
-    h = image(im);
-    set(h, 'CDataMapping', 'scaled');
-    set(gca, 'Units', 'Normalized', 'Position', [0.13 0.15 0.775 0.75]);
-
+    
     % create threshold display window
     htf = uicontrol(gcf, ...
             'style', 'text', ...
@@ -75,6 +85,18 @@ function ithresh(im, t)
             'string', num2str(t) ...
         );
 
+    
+    if nargout > 0
+        % create quit button if outputs required
+        donebutton = uicontrol(gcf, ...
+            'style', 'pushbutton', ...
+            'units',  'norm', ...
+            'pos', [.02 .935 .1 .05], ...
+            'callback', 'uiresume', ...
+            'string', 'done' ...
+            );
+    end
+    
     % create user data structure
     %  ud.n           the number of colors in the color map
     %  ud.htf handle  for the threshold display
@@ -90,21 +112,55 @@ function ithresh(im, t)
     end
 
     % create slider
-    uicontrol(gcf,'Style','Slider', ...
-        'Units','norm','Position',[0.04 0 0.9 .07], ...
+    slider = uicontrol(gcf,'Style','Slider', ...
+        'Units','norm', ...
+        'Position',[0.04 0 0.9 .07], ...
+        'Min', 0.0, ...
+        'Max', 1.0, ...
         'UserData', ud, ...
         'Value', t, ...
+        'Tag', 'threshold', ...
         'Callback', @thresh_callback );
-
+    
     set_threshold(t, ud);
+
+    %{
+    THIS CODE BLOCK WORKS FINE IN SERIALLINK.TEACH BUT NOT HERE...
+    % if findjobj exists use it, since it lets us get continous callbacks while
+    % a slider moves
+    if (exist('findjobj', 'file')>0) && ~ispc
+        disp('using findjobj');
+        drawnow
+        jh = findjobj(slider, 'nomenu')
+        jh.AdjustmentValueChangedCallback = @(src,event)thresh_callback(src, event);
+    end
+    %}
+    
     set(gcf, 'name', 'ithresh');
+    
+    if nargout > 0
+        % wait till the done button is pushed
+        uiwait();
+        
+        % then return arguments as requested
+        t = get(slider, 'Value');
+        if ~isinf(ud.max)
+            t = t * ud.max;
+        end
+        if nargout >= 1
+            imt = im > t;
+        end
+        if nargout == 2
+            thresh = t;
+        end
+    end
 end
 
 % invoked on a GUI event
-function thresh_callback(obj, events, handles)
+function thresh_callback(obj, events)
 
     % get slider value
-    t = get(obj, 'Value')
+    t = get(obj, 'Value');
 
     ud = get(obj, 'UserData');
     set_threshold(t, ud);
@@ -114,7 +170,7 @@ function set_threshold(t, ud)
     % threshold is changed by manipulating the color map
 
     % round the threshold up to an integer color map index
-    v = round(t*ud.n);
+    v = floor(t*(ud.n-1)+1);
 
     % create the color map
     if ud.true_high
