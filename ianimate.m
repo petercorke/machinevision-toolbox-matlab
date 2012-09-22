e67122ea31c604dae85d3767fe80db9a4691da34
Peter Corke <peter.i.corke@gmail.com>
2012-09-22 22:44:50 +0000

    Added 'title' option to allow frame titles to be individually specified Now displays color image sequence Doco upgrade

--- ianimate.m
+++ ianimate.m
@@ -1,12 +1,14 @@
 %IANIMATE Display an image sequence
 %
-% IANIMATE(IM, OPTIONS) displays a greyscale image sequence IM (HxWxN)
-% where N is the number of frames in the sequence.
+% IANIMATE(IM, OPTIONS) displays a greyscale image sequence IM (HxWxN) or
+% a color image sequence IM (HxWx3xN) where N is the number of frames in 
+% the sequence.
 %
-% IANIMATE(IM, FEATURES, OPTIONS) displays a greyscale image sequence IM with
-% point features overlaid.  FEATURES (Nx1) cell array whose elements are
-% vectors of feature objects.  The feature is plotted using the object's plot
-% method and additional options are passed through to that method.
+% IANIMATE(IM, FEATURES, OPTIONS) as above but with point features overlaid.
+% FEATURES (Nx1) is a cell array whose elements are vectors of feature 
+% objects for the corresponding frames of IM.  The feature is plotted 
+% using the feature object's plot method and additional options are passed 
+% through to that method.
 %
 % Examples::
 %
@@ -15,7 +17,7 @@
 %
 % Animate image sequence with overlaid corner features:
 %     c = icorner(im, 'nfeat', 200);  % computer corners
-%     ianimate(seq, features, 'gs');  % features shown as green squares
+%     ianimate(seq, c, 'gs');  % features shown as green squares
 %
 % Options::
 %  'fps',F       set the frame rate (default 5 frames/sec)
@@ -23,6 +25,18 @@
 %  'movie',M     save the animation as a series of PNG frames in the folder M
 %  'npoints',N   plot no more than N features per frame (default 100)
 %  'only',I      display only the I'th frame from the sequence
+%  'title',T     displays the specified title on each frame, T is a cell
+%                array (1xN) of strings.
+%
+% Notes::
+% - If titles are not specified the title is "frame N"
+% - If the 'movie' is used the frames can be converted to a movie
+%   using a utility like ffmpeg, for instance:
+%
+%          ffmpeg -i *.png -r 5 movie.mp4
+%
+%   or to set the bit rate explicitly
+%          ffmpeg -i *.png -b:v 64k movie.mp4
 %
 % See also PointFeature, IHARRIS, ISURF, IDISP.
 
@@ -54,6 +68,7 @@ function ianimate(im, varargin)
     opt.npoints = 100;
     opt.only = [];
     opt.movie = [];
+    opt.title = [];
 
     [opt, arglist]  = tb_optparse(opt, varargin);
 
@@ -64,36 +79,52 @@ function ianimate(im, varargin)
     
     clf
     pause on
-    colormap(gray(256));
     
     if ~isempty(opt.movie)
         mkdir(opt.movie);
         framenum = 1;
     end
 
+    if ndims(im) == 3
+        nframes = size(im, 3);
+        color = false;
+    else
+        nframes = size(im, 4);
+        color = true;
+    end
+    
     while true
-        for i=1:size(im,3)
+        for i=1:nframes
             if opt.only ~= i
                 continue;
             end
-            image(im(:,:,i), 'CDataMapping', 'Scaled');
+            
+            if color
+                image(im(:,:,:,i)); 
+            else
+                colormap(gray(256));
+                image(im(:,:,i), 'CDataMapping', 'Scaled');
+            end
             if ~isempty(points)
                 f = points{i};
                 n = min(opt.npoints, length(f));
                 f(1:n).plot(arglist{:});
             end
-            title( sprintf('frame %d', i) );
+            if isempty(opt.title)
+                title( sprintf('frame %d', i) );
+            else
+                title( opt.title{i} );
+            end
 
             if opt.only == i
                 return;
             end
             if isempty(opt.movie)
-                            pause(1/opt.fps);
+                pause(1/opt.fps);
             else
                 f = getframe;
                 imwrite(f.cdata, sprintf('%s/%04d.png', opt.movie, framenum));
-                framenum = framenum+1;
-            
+                framenum = framenum+1;    
             end
         end
 
