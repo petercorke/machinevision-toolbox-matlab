026b191cff174dc4774b9ab9a2535a43e9d74ab4
Peter Corke <peter.i.corke@gmail.com>
2012-09-22 23:36:03 +0000

    Pass arguments through to interp2 Doco upgrade

--- iwarp.m
+++ iwarp.m
@@ -1,15 +1,19 @@
 %IWARP Generalized warp
 %
-% OUT = IWARP(IM, U, V) returns a warped image OUT (WxH) whose
-% output pixels are given by the correspding coordinates of
-% U and V (both WxH).  That is, the value of OUT(I,J) is given
-% by IM(X,Y) where X=U(I,J) and Y=V(I,J).  In general X and Y are
+% OUT = IWARP(IM, U, V) is a warped image (WxH) corresponding to
+% IM (WxH).  The output pixels are taken from IM at the coordinates given
+% by U and V (both WxH).  That is, the value of OUT(I,J) is given by the
+% value of IM(X,Y) where X=U(I,J) and Y=V(I,J).  In general X and Y are
 % not integers and the value is found by interpolation.
+%
+% OUT = IWARP(IM, U, V, OPTIONS) as above but OPTIONS are passed to
+% the MATLAB function INTERP2 to select interpolation mode or to set
+% the value of unmapped pixels.
 %
 % See also HOMWARP, INTERP2
 
-function out = iwarp(im, U, V)
+function out_ = iwarp(im, U, V, varargin)
 
     [Ui,Vi] = imeshgrid(im);
 
-    out = interp2(Ui, Vi, im, U, V);
+    out = interp2(Ui, Vi, im, U, V, varargin{:});
