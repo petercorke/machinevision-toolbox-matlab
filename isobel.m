05e41284a8de6d200e0ee7fef78b0b2f2568b563
Peter Corke <peter.i.corke@gmail.com>
2012-09-22 23:30:19 +0000

    'smooth' and 'size' options code cleanup use iconv instead of conv2 doco upgrade

--- isobel.m
+++ isobel.m
@@ -2,24 +2,29 @@
 %
 % OUT = ISOBEL(IM) is an edge image computed using the Sobel edge operator
 % applied to the image IM.  This is the norm of the vertical and horizontal 
-% gradients at each pixel.  The Sobel kernel is:
-%      | -1  0  1|
-%      | -2  0  2|
-%      | -1  0  1|
+% gradients at each pixel.  The Sobel horizontal gradient kernel is:
+%        | -1  0  1|
+%        | -2  0  2|
+%        | -1  0  1|
+%
+% and the vertical gradient kernel is the transpose.
+%
+% [GX,GY] = ISOBEL(IM) as above but returns the gradient images rather than
+% the gradient magnitude.
 %
 % OUT = ISOBEL(IM,DX) as above but applies the kernel DX and DX' to compute
 % the horizontal and vertical gradients respectively.
 %
-% [GX,GY] = ISOBEL(IM) as above but returns the gradient images.
-%
-% [GX,GY] = ISOBEL(IM,DX) as above but returns the gradient images.
+% [GX,GY] = ISOBEL(IM,DX) as above but returns the gradient images rather than
+% the gradient magnitude.
 %
 % Notes::
 % - Tends to produce quite thick edges.
 % - The resulting image is the same size as the input image.
+% - If the kernel DX is provided it can be of any size, not just 3x3, and
+%   could be generated using KDGAUSS.
 %
-% See also KSOBEL, ICANNY, ICONV.
-
+% See also KSOBEL, KDGAUSS, ICANNY, ICONV.
 
 
 % Copyright (C) 1993-2011, by Peter I. Corke
@@ -39,21 +44,35 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function [o1,o2] = isobel(i, Dx)
+function [o1,o2] = isobel(im, varargin)
 
-    if nargin < 2,
-        sv = -[ -1 -2 -1
+    % convert color image to greyscale
+    if size(im) > 1
+        im = imono(im);
+    end
+    
+    opt.size = {'same', 'full', 'valid'};
+    opt.smooth = [];
+    [opt,args] = tb_optparse(opt, varargin);
+    
+    
+    if size(im, 3) > 1
+        error('MVTB:isobel:badarg', 'not defined for multiplane image');
+    end
+    if length(args) == 0
+        Dx = [ -1 -2 -1
             0 0 0
             1 2 1];
-        sh = sv';
     else
-        % use a smoothing kernel if sigma specified
-        sh = Dx;
-        sv = Dx';
+        % use the passed horizontal gradient kernel
+        Dx = args{1};
+    end
+    if ~isempty(opt.smooth)
+        Dx = kdgauss(opt.smooth);
     end
 
-    ih = conv2(i, sh, 'same');
-    iv = conv2(i, sv, 'same');
+    ih = iconv(im, Dx, opt.size);
+    iv = iconv(im, Dx', opt.size);
 
     % return grandient components or magnitude
     if nargout == 1,
