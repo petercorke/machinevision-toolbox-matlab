18f815dc0bb1333acc4b460cc397fb2657ed060e
Peter Corke <peter.i.corke@gmail.com>
2012-09-23 00:52:05 +0000

    Code tidyup. Add includes for openSurf, and VLfeat Sift implementations

--- startup_mvtb.m
+++ startup_mvtb.m
@@ -5,22 +5,34 @@ addpath( fullfile(tbpath, 'examples') );
 addpath( fullfile(tbpath, 'images') );
 addpath( fullfile(tbpath, 'mex') );
 % add the contrib code to the path
-p = fullfile(tbpath, 'contrib/vgg');
+p = fullfile(rvcpath, 'contrib/vgg');
 if exist(p)
     addpath( p );
     disp([' - VGG contributed code (' p ')']);
 end
-p = fullfile(tbpath, 'contrib/EPnP/EPnP');
+p = fullfile(rvcpath, 'contrib/EPnP');
 if exist(p)
     addpath( p );
     disp([' - EPnP contributed code (' p ')']);
 end
-p = fullfile(tbpath, ['contrib/vlfeat-0.9.9/toolbox/mex/' mexext]);
+p = fullfile(rvcpath, ['contrib/vlfeat-0.9.9/toolbox/mex/' mexext]);
 if exist(p)
     addpath( p );
     disp([' - VLFeat contributed code (' p ')']);
+    
+    p = fullfile(rvcpath, 'contrib/sift');
+    if exist(p)
+        addpath( p );
+        disp([' - VLFeat SIFT wrapper (' p ')']);
+    end
 end
-p = fullfile(tbpath, 'contrib/graphseg');
+
+p = fullfile(rvcpath, 'contrib/surf');
+if exist(p)
+    addpath( p );
+    disp([' - OpenSURF contributed code + wrapper (' p ')']);
+end
+p = fullfile(rvcpath, 'contrib/graphseg');
 if exist(p)
     addpath( p );
     disp([' - graphseg contributed code (' p ')']);
