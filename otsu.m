7aac541d8ec8d350f80f722ff7f2b0cc7620a67d
Peter Corke <peter.i.corke@gmail.com>
2014-04-06 02:02:20 +0000

    Added option for Gaussian weighted valley emphasis method

--- otsu.m
+++ otsu.m
@@ -8,6 +8,10 @@
 %     t = otsu(im);
 %     idisp(im >= t);
 %
+% Options::
+% 'levels',N    Number of grey levels to use if image is float (default 256)
+% 'valley',S    Standard deviation for the Gaussian weighted valley emphasis option
+%
 % Notes::
 % - Performance for images with non-bimodal histograms can be quite poor.
 %
@@ -16,6 +20,13 @@
 %  N. Otsu
 %  IEEE Trans. Systems, Man and Cybernetics
 %  Vol SMC-9(1), Jan 1979, pp 62-66
+%
+%  An improved method for image thresholding on the valley-emphasis method
+%  H-F Ng, D. Jargalsaikhan etal
+%  Signal and Info Proc. Assocn. Annual Summit and Conf (APSIPA)
+%  2013
+%  pp 1-4
+%  
 %
 % See also NIBLACK, ITHRESH.
 
@@ -37,23 +48,33 @@
 % You should have received a copy of the GNU Leser General Public License
 % along with MVTB.  If not, see <http://www.gnu.org/licenses/>.
 
-function t = otsu(im, N)
+function t = otsu(im, varargin)
 
-    if nargin < 2
-        N = 255;
-    end
+    opt.levels = 256;
+    opt.valley = 0;
+    opt = tb_optparse(opt, varargin);
+    
     n = prod(size(im));
     nb = 0;
     no = n;
     ub = 0;
 
-    % convert image to discrete values [0,N]
+    % convert image to discrete values [0,N-1]
     if isfloat(im)
-        im2 = round(im*N);
+        N = opt.levels;
+        im2 = round(im*(N-1));
     else
         im2 = im;
+        N = double(intmax(class(im))) + 1;
     end
 
+    % notation as per the Otsu paper
+    %
+    % uo \mu_o object
+    % ub \mu_b background
+    % no number of pixels in object
+    % nb number of pixels in background
+    % s2b  \sigma^2_b
     h = histc(im2(:), 0:N);
     uo = sum(im2(:))/n;
 
@@ -71,8 +92,19 @@ function t = otsu(im, N)
 
         ub = (ub*nb + nt*(T-1)) / nb_new;
         uo = (uo*no - nt*(T-1)) / no_new;
+        
+        if opt.valley == 0
+            W = 1;
+        else
+            % valley emphasis option
+            W = 0;
+            for i=1:N
+                W =  W + h(i)/n * exp((i-T)/2/opt.valley^2);
+            end
+            W = 1 - W;
+        end
 
-        s2b(T) = nb*no*(ub - uo)^2;
+        s2b(T) = W * nb*no*(ub - uo)^2;
 
         %fprintf('%d %d %f %f %f\n', nb, no, ub, uo, s2b(T));
         nb = nb_new;
