688bc6b51d405596a407a8d6a985e4772e7b4f5a
Peter Corke <peter.i.corke@gmail.com>
2015-12-04 11:31:04 +0000

    Faster code with less ragged results. Based on code from Berkeley

--- icanny.m
+++ icanny.m
@@ -1,6 +1,6 @@
 %ICANNY	Canny edge detection
 %
-% E = ICANNY(IM, OPTIONS) is an edge image obtained using the Canny edge 
+% E  =  ICANNY(IM, OPTIONS) is an edge image obtained using the Canny edge
 % detector algorithm.  Hysteresis filtering is applied to the gradient
 % image: edge pixels > th1 are connected to adjacent pixels > th0, those
 % below th0 are set to zero.
@@ -11,7 +11,7 @@
 %  'th1',T   set the upper hysteresis threshold (default 0.5 x strongest edge)
 %
 % Reference::
-% - "A Computational Approach To Edge Detection", 
+% - "A Computational Approach To Edge Detection",
 %   J. Canny,
 %   IEEE Trans. Pattern Analysis and Machine Intelligence, 8(6):679–698, 1986.
 %
@@ -21,55 +21,52 @@
 % - If th1 is zero then no hysteresis filtering is performed.
 % - A color image is automatically converted to greyscale first.
 %
-% Author::
-% Oded Comay, Tel Aviv University, 1996-7.
 %
 % See also ISOBEL, KDGAUSS.
 
-function E = icanny(I, varargin)
+% http://robotics.eecs.berkeley.edu/~sastry/ee20/cacode.html
 
+function E  =  icanny(I, varargin)
+    
     % convert color image to greyscale
     if iscolor(I)
-        I = imono(I);
+        I  =  imono(I);
     end
-
-    opt.sd = 1;
-    opt.th0 = 0.1;
-    opt.th1 = 0.5;
-
-    opt = tb_optparse(opt, varargin);
-
-    x= -5*opt.sd:opt.sd*5; 
-    g = exp(-0.5/opt.sd^2*x.^2); 		% Create a normalized Gaussian
-    g = g(g>max(g)*.005); g = g/sum(g(:));
-    dg = diff(g);				% Gaussian first derivative
-
-    dx = abs(conv2(I, dg, 'same'));		% X/Y edges
-    dy = abs(conv2(I, dg', 'same'));
-
-    [ny, nx] = size(I);
-                        % Find maxima 
-    dy0 = [dy(2:ny,:); dy(ny,:)]; dy2 = [dy(1,:); dy(1:ny-1,:)];
-    dx0 = [dx(:, 2:nx) dx(:,nx)]; dx2 = [dx(:,1) dx(:,1:nx-1)];
-    peaks = find((dy>dy0 & dy>dy2) | (dx>dx0 & dx>dx2));
-    e = zeros(size(I));
-    e(peaks) = sqrt(dx(peaks).^2 + dy(peaks).^2); 
-
-    e(:,2)    = zeros(ny,1);    e(2,:) = zeros(1,nx);	% Remove artificial edges
-    e(:,nx-2) = zeros(ny,1); e(ny-2,:) = zeros(1,nx);
-    e(:,1)    = zeros(ny,1);    e(1,:) = zeros(1,nx);
-    e(:,nx)   = zeros(ny,1);   e(ny,:) = zeros(1,nx);
-    e(:,nx-1) = zeros(ny,1); e(ny-1,:) = zeros(1,nx);
-    e = e/max(e(:));
-
-    if opt.th1  == 0, E = e; return; end			 % Perform hysteresis
-    E(ny,nx) = 0;
-
-    p = find(e >= opt.th1);
-    while length(p) 
-      E(p) = e(p);
-      e(p) = zeros(size(p));
-      n = [p+1 p-1 p+ny p-ny p-ny-1 p-ny+1 p+ny-1 p+ny+1]; % direct neighbors
-      On = zeros(ny,nx); On(n) = n;
-      p = find(e > opt.th0 & On);
-    end
+    
+    opt.sigma  =  1;
+    opt.th1  =  0.1;
+    
+    opt  =  tb_optparse(opt, varargin);
+    
+    
+    % compute gradients
+    dg  =  kdgauss(opt.sigma);
+    Ix  =  abs(conv2(I, dg, 'same'));		% X/Y edges
+    Iy  =  abs(conv2(I, dg', 'same'));
+    
+    
+    % Norm of the gradient (Combining the X and Y directional derivatives)
+    grad = sqrt(Ix.*Ix+Iy.*Iy);
+    
+    
+    % Thresholding
+    gmax = max(grad(:));
+    %gmin = min(grad(:));
+    gmin = 0;
+    level = opt.th1*(gmax-gmin)+gmin;
+    Ithresh = max(grad,level);
+    
+    
+    E = ones(size(I)) * gmin;
+    
+    [U,V] = imeshgrid(Ithresh);
+    k = Ithresh(:) > level;
+    Z1 = interp2(U, V, Ithresh, U(k)+Ix(k)./grad(k), V(k)+Iy(k)./grad(k) );
+    Z2 = interp2(U, V, Ithresh, U(k)-Ix(k)./grad(k), V(k)-Iy(k)./grad(k) );
+    
+    k2 = Ithresh(k) >= Z1 & Ithresh(k) >= Z2;
+    i = find(k);
+    i = i(k2);
+    E(i) = gmax;
+    
+end
