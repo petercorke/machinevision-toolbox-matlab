be3e799c50c74538b8bc9f22194a833f68068c5a
Peter Corke <peter.i.corke@gmail.com>
2017-04-16 15:45:22 +1000

    Doco updates Only sort into into strength descending order if strength is provided by the underlying SIFT implementation

--- isift.m
+++ isift.m
@@ -5,9 +5,22 @@
 % image IM.
 %
 % Options::
-% 'nfeat',N      set the number of features to return (default Inf)
-% 'suppress',R   set the suppression radius (default 0)
-% 'id',V         set the image_id of all features
+% 'nfeat',N          set the number of features to return (default Inf)
+% 'suppress',R       set the suppression radius (default 0)
+% 'id',V             set the image_id of all features
+%-
+% 'Octaves',N        set the number of octaves of the DoG scale space
+% 'Levels',L         set the number of levels per octave of the DoG scale space (3)
+% 'FirstOctave', N   set the index of the first octave of the DoG scale space (0)
+% 'PeakThresh',N     set the peak selection threshold (0)
+% 'EdgeThresh',N     set the non-edge selection threshold (10)
+% 'NormThresh', N    set the minimum l2-norm of the descriptors before normalization. 
+%                    Descriptors below the threshold are set to zero (-inf)
+% 'Magnif',N         set the descriptor magnification factor (3)
+% 'WindowSize',N     set the variance of the Gaussian window (2)
+%
+% See VLFeat.org for more details.
+%
 %
 % Properties and methods::
 %
@@ -30,13 +43,18 @@
 % See SiftPointFeature and PointFeature classes for more details.
 % 
 % Notes::
-% - Greyscale images only, double or integer pixel format.
-% - Features are returned in descending strength order.
-% - Wraps a MEX file from www.vlfeat.org
-% - Corners are processed in order from strongest to weakest.
+% - Greyscale images only.
 % - If IM is HxWxN it is considered to be an image sequence and F is a cell 
 %   array with N elements, each of which is the feature vectors for the 
 %   corresponding image in the sequence.
+% - If VLFeat is installed uses vl_feat()
+%   - at least 5x faster
+%   - does not return feature strength
+%   - does not sort features by strength
+%   - 'nfeat' option cannot be used, adjust 'PeakThresh' to control the
+%     number of features
+% - Default MEX implementation by Andrea Vedaldi (2006).
+%   - Features are returned in descending strength order.
 % - The SIFT algorithm is covered by US Patent 6,711,293 (March 23, 2004) held
 %   by the Univerity of British Columbia.
 % - ISURF is a functional equivalent.
@@ -128,16 +146,22 @@ function features = isift(im, varargin)
     fprintf('%d corners found (%.1f%%), ', numcols(key), ...
         numcols(key)/prod(size(im))*100);
 
-    % sort into descending order of corner strength
-    [z,k] = sort(key(5,:), 'descend');
-    key = key(:,k);
-    desc = desc(:,k);
+    % sort into descending order of corner strength if provided
+    if numrows(key) > 4
+        [z,k] = sort(key(5,:), 'descend');
+        key = key(:,k);
+        desc = desc(:,k);
+        n = min(opt.nfeat, numcols(key));
+
+    else
+        key = [key; NaN*ones(1, numcols(key))];
+        n = numcols(key);
+        if ~isinf(opt.nfeat)
+            warning('cannot limit number of returned features using vl_feat');
+        end
+    end
 
     % allocate storage for the objects
-    n = min(opt.nfeat, numcols(key));
-
-    % sort into decreasing strength
-
 
     features = [];
     i = 1;
