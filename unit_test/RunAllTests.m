d5b12af181e83189772261031f4c4345767ab19e
Peter Corke <peter.i.corke@gmail.com>
2019-08-11 09:45:47 +1000

    Setup for Travis CI

---
+++ unit_test/RunAllTests.m
@@ -0,0 +1,58 @@
+% run the tests for Travis CI
+%
+% Travis file system looks like this:
+%    ./           ** MVTB is unpacked at this level, not its own folder
+%    ./images
+%    ./unit_test  ** WORKING folder
+%	 ./lib/toolbox-common
+%    ./lib/spatial-math-toolbox
+
+%% set up the test runner
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+import matlab.unittest.TestRunner
+
+suite = testsuite('IncludeSubfolders', false);
+runner = TestRunner.withTextOutput;
+
+% add a coverage report
+reportFile = fullfile('..', 'coverage.xml');
+reportFormat = CoberturaFormat(reportFile);
+plugin = CodeCoveragePlugin.forFolder('..', 'Producing',reportFormat);
+runner.addPlugin(plugin);
+
+%% compile some codecoverage
+
+
+%% setup the path
+fprintf('---------------------------------- Setup path ------------------------------------\n')
+
+% for other toolboxes
+addpath ../lib/toolbox-common-matlab
+addpath ../lib/spatial-math
+
+% for MVTB
+addpath ..
+addpath ../data
+addpath ../images
+
+
+%path
+system('mount');
+
+originalDir = pwd
+
+% build the MEX file
+fprintf('---------------------------------- Build MEX files ------------------------------------\n')
+cd ../mex
+make
+
+cd(originalDir)
+
+%% Run all unit tests in my repository
+fprintf('---------------------------------- Run the unit tests ------------------------------------\n')
+
+results = runner.run(suite);
+
+%% Assert no tests failed
+assert(all(~[results.Failed]));
